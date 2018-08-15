8f77d66650a80efc5c8708a95e2f3c3e26569037
unknown <migue_danxor@hotmail.com>
2018-08-15 17:05:56 -0500

    ¨caca¨

---
+++ Assets/Scripts/Gear.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Gear : MonoBehaviour {
+	void Update () {
+        gameObject.transform.Rotate(transform.forward *-30* Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Times.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Times : MonoBehaviour {
+   public GameObject horas;
+    public GameObject minutos;
+    int h;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        h = Mathf.Abs(System.DateTime.Now.Hour - 12);
+        
+            horas.transform.rotation=Quaternion.Euler(new Vector3( 0f,0f, h*-30 +90 ));
+            minutos.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, DateTime.Now.Minute*-6+90 ));
+        Debug.Log(DateTime.Now.Minute);
+
+
+    }
+}
